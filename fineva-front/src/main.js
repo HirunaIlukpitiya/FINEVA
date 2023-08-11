84acca01fa31d92bc05806b20798d2c5543cf2d7
HirunaIlukpitiya <33183435+HirunaIlukpitiya@users.noreply.github.com>
2023-08-11 10:16:03 +0530

    updated project 08.11

--- fineva-front/src/main.js
+++ fineva-front/src/main.js
@@ -1,11 +1,10 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import './main.css'
 import router from './router'
+import store from './store'
 
 const app = createApp(App)
-
 app.use(router)
-
+app.use(store)
 app.mount('#app')
--- fineva-front/src/router/index.js
+++ fineva-front/src/router/index.js
@@ -1,21 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path:"/",
+      name:"Home",
+      component:()=>import('../views/Home.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path:"/SignIn",
+      name:"SignIn",
+      component:()=>import('../views/Signin.vue')
+    },
+    {
+      path:"/driverReg",
+      name:"driverReg",
+      component:()=>import('../views/driverReg.vue')
+    },
+    {
+      path:"/policeReg",
+      name:"policeReg",
+      component:()=>import('../views/policeReg.vue')
+    },
+    {
+      path:"/policeHome",
+      name:"policeHome",
+      component:()=>import('../views/policeHome.vue')
     }
   ]
 })
