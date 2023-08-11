4279c1f18560a6e5b93a111ee72977c445f8bc26
HirunaIlukpitiya <33183435+HirunaIlukpitiya@users.noreply.github.com>
2023-08-11 14:05:30 +0530

    update project 08.11

--- fineva-front/src/App.vue
+++ fineva-front/src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import {RouterView } from 'vue-router'
+import policeHome from './views/policeHome.vue'
 </script>
 
 <template>
   <RouterView />
+  <policeHome/>
 </template>
 
 <style>
