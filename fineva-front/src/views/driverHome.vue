d16472df49b16a22d13fd193d6143333cbebbf92
hirunaIlukpitiya <hirunalakshan00@gmail.com>
2023-08-28 03:27:27 +0530

    driver home add

---
+++ fineva-front/src/views/driverHome.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  methods: {},
+};
+</script>
+
+<template>
+  <div class="min-h-screen relative bg-[url('../../src/assets/image/BG.png')]">
+    <nav class="w-full lg:h-[5rem] h-[2rem] bg-white/10 backdrop-blur sticky top-0 grid-cols-2">
+    <div class="">
+      <div class="mx-[20px] ">
+        <span
+          class="text-wt font-bold text-[15px] lg:text-[65px] xl:text-[40px]"
+          style="font-family: 'Times New Roman', Times, serif"
+          >FINEV</span
+        ><span
+          class="text-ylv font-bold text-[15px] lg:text-[65px] xl:text-[40px]"
+          style="font-family: 'Times New Roman', Times, serif"
+          >A&nbsp;</span
+        >
+        <span
+          class="text-wt text-[15px] lg:text-[35px] xl:text-[40px]"
+          style="font-family: 'Times New Roman', Times, serif"
+          >&nbsp;&nbsp; D R I V E R</span
+        >
+      </div>
+    </div>
+      <div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style></style>
